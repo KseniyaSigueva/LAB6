9ff3a3eba502052a566aabb8547c636483e5df5b
KseniyaSigueva <86232345+KseniyaSigueva@users.noreply.github.com>
2021-06-21 15:39:58 +0500

    Add files via upload

---
+++ LAB6/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР6
+{
+    [Serializable]
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Firm { get; set; }
+        public string Owner { get; set; }
+        
+        public Car()
+        {
+
+        }
+        public Car(string name, string firm, string owner)
+        {
+            Name = name;
+            Firm = firm;
+            Owner = owner;
+        }
+    }
+}
---
+++ LAB6/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person()
+    {
+
+    }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
